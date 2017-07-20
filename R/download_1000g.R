454c523cb7dfe8cf40ddac980dd19617f7df98ac
James Diao <diao.james@gmail.com>
2017-07-20 17:30:50 -0400

    Stable download_1000g and import_file_1000g functions

--- R/download_1000g.R
+++ R/download_1000g.R
@@ -2,8 +2,8 @@
 #'
 #' This function downloads a gene's VCF from the 1000 Genomes FTP based on its transcription region
 #' as listed in the UCSC Genome database.
-#' @usage download_1000g(gene, download)
-#' @param gene character; name of a gene indexed in refGene
+#' @usage download_1000g(genes, download)
+#' @param genes character; name of a gene(s) indexed in refGene
 #' @param download logical; indicates whether to download the file. 
 #' FALSE still returns transcription coordinates and RefGene details from UCSC. 
 #' @examples 
@@ -11,44 +11,58 @@
 #' download_output <- sapply(ACMG.panel, function(gene) download_1000g(gene, download = T)) %>% t
 #' @export
 
-download_1000g <- function(gene, download) {
+download_1000g <- function(genes, download) {
   if (missing(download)) {
     download <- TRUE
   }
-  query <- connect_UCSC()
-  #for tracking: #gene %>% paste(which(ACMG.panel==gene)) %>% paste(length(ACMG.panel), sep = "/") %>% print
-  success <- FALSE
-  refGene <- sprintf("select * from refGene where name2 = \"%s\" limit 20", gene) %>% query
-  UCSC <- select(refGene, name, chrom, start = txStart, end = txEnd)
-  if (nrow(UCSC) == 0) { #No hit on refGene
-    return(rep("NOT_FOUND",5) %>% setNames(c("name","chrom","start","end","downloaded")))
-  } else {
-    if (nrow(UCSC) > 1) #Multiple hits: take the widest range
-      UCSC <- UCSC[which.max(UCSC$end-UCSC$start),]
-    if (download) {
-      # gets [n] from chr[n]
-      chrom.num <- strsplit(UCSC$chrom, split = "chr")[[1]][2]
-      # different version for chromosomes X and Y
-      version <- switch(chrom.num, "X" = "shapeit2_mvncall_integrated_v1b",
-                        "Y" = "integrated_v2a", "shapeit2_mvncall_integrated_v5a")
-      dir <- system.file("extdata", "1000G/", package = "clinvaR")
-      command <- paste("tabix -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.%s.",
-                       "phase3_%s.20130502.genotypes.vcf.gz %s:%s-%s > %s%s_genotypes.vcf", sep = "")
-      sprintf(command, UCSC$chrom, version, chrom.num, UCSC$start, UCSC$end, dir, gene) %>% system
-      #url <- paste0("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.%s.",
-      #              "phase3_%s.20130502.genotypes.vcf.gz")
-      #dir <- system.file("extdata", "1000G/", package = "clinvaR")
-      #sprintf(url, UCSC$chrom, version, chrom.num, UCSC$start, UCSC$end, gene) %>% 
-      #  download.file(destfile = sprintf('%s%s_genotypes.vcf', dir, gene), method = "internal")
-      # Checks whether the file exists and has non-zero size
-      exists <- grepl(paste0(gene,"_genotypes.vcf"), 
-                      system(sprintf("ls %s", dir), intern = T)
-                      ) %>% any
-      file.stat <- unlist(sprintf("stat %s_genotypes.vcf", gene) %>% 
-                      system(intern = T) %>% strsplit(" "))
-      file.size <- file.stat[grep("Size",file.stat)+1] %>% as.integer
-      success <- exists & file.size > 0
+  download_output <- sapply(genes, function(gene) {
+    query <- connect_UCSC()
+    #for tracking: #gene %>% paste(which(ACMG.panel==gene)) %>% paste(length(ACMG.panel), sep = "/") %>% print
+    success <- FALSE
+    refGene <- sprintf("select * from refGene where name2 = \"%s\" limit 20", gene) %>% query
+    UCSC <- select(refGene, name, chrom, start = txStart, end = txEnd)
+    if (nrow(UCSC) == 0) { #No hit on refGene
+      return(rep("NOT_FOUND",5) %>% setNames(c("name","chrom","start","end","downloaded")))
+    } else {
+      if (nrow(UCSC) > 1) #Multiple hits: take the widest range
+        UCSC <- UCSC[which.max(UCSC$end-UCSC$start),]
+      if (download) {
+        # gets [n] from chr[n]
+        chrom.num <- strsplit(UCSC$chrom, split = "chr")[[1]][2]
+        # different version for chromosomes X and Y
+        version <- switch(chrom.num, "X" = "shapeit2_mvncall_integrated_v1b",
+                          "Y" = "integrated_v2a", "shapeit2_mvncall_integrated_v5a")
+        dir <- system.file("extdata", "1000G/", package = "clinvaR")
+        file.name <- sprintf(
+                        fmt = paste0("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/",
+                               "ALL.%s.phase3_%s.20130502.genotypes.vcf.gz"), 
+                        UCSC$chrom, version
+                     )
+        output <- tabix.read(tabixFile = file.name, 
+                             tabixRange = sprintf('%s:%s-%s', chrom.num, UCSC$start, UCSC$end)
+                             )
+        output <- read.table(text = paste(output, collapse = "\n"), header = F, stringsAsFactors = F, 
+                            comment.char = "", quote = "", sep = "\t")
+        saveRDS(output, sprintf('%s%s_genotypes_vcf.rds', dir, gene))
+        #command <- paste("tabix -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.%s.",
+        #                 "phase3_%s.20130502.genotypes.vcf.gz %s:%s-%s > %s%s_genotypes.vcf", sep = "")
+        #sprintf(command, UCSC$chrom, version, chrom.num, UCSC$start, UCSC$end, dir, gene) %>% system
+        #url <- paste0("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.%s.",
+        #              "phase3_%s.20130502.genotypes.vcf.gz")
+        #dir <- system.file("extdata", "1000G/", package = "clinvaR")
+        #sprintf(url, UCSC$chrom, version, chrom.num, UCSC$start, UCSC$end, gene) %>% 
+        #  download.file(destfile = sprintf('%s%s_genotypes.vcf', dir, gene), method = "internal")
+        # Checks whether the file exists and has non-zero size
+        exists <- grepl(paste0(gene,"_genotypes_vcf.rds"), 
+                        system(sprintf("ls %s", dir), intern = T)
+                        ) %>% any
+        file.stat <- unlist(sprintf("stat %s%s_genotypes_vcf.rds", dir, gene) %>% 
+                        system(intern = T) %>% strsplit(" "))
+        file.size <- file.stat[grep("Size",file.stat)+1] %>% as.integer
+        success <- exists & file.size > 0
+      }
     }
-  }
-  return(c(UCSC,"downloaded" = success))
+    return(c(UCSC,"downloaded" = success))
+  })
+  return(download_output)
 }
--- R/import_VCF.R
+++ R/import_VCF.R
@@ -70,7 +70,7 @@ import_file_exac <- function(gene, dataset, path) {
 #'
 #' This function imports a gene's VCF and extracts attributes from INFO 
 #' into a specified format for other clinvaR analyses
-#' @usage import_file_1000g(gene, dataset, path)
+#' @usage import_file_1000g(genes, dataset, path)
 #' @param gene character; any of the pre-stored genes in extdata/1000G
 #' Not case-sensitive. Defaults to 'gnomAD'.
 #' @param path character; path to an importable VCF. Overrides gene input. 
@@ -81,61 +81,61 @@ import_file_exac <- function(gene, dataset, path) {
 #' }
 #' #ACMG.1000g[duplicated(ACMG.1000g$VAR_ID),1:8] # Display and remove duplicates
 #' ACMG.1000g <- ACMG.1000g[!duplicated(ACMG.1000g$VAR_ID),]
-#' #@export
+#' @export
 
-import_file_1000g <- function(gene, path) {
-  if (missing(path)) {
-    output <- sprintf("%s%s/%s_%s.csv", dir, dataset, dataset, gene) %>% 
-      read.csv(stringsAsFactors = FALSE)
-  } else {
-    output <- read.csv(path, stringsAsFactors = FALSE)
+import_file_1000g <- function(genes) {
+  
+  temp_function <- function(gene) {
+    path <- sprintf("%s/1000G/%s_genotypes_vcf.rds", dir, gene)
+    output <- readRDS(path)
+    colnames(output) <- header
+    
+    #Remove all single alt indels
+    output <- output %>% filter(nchar(REF)==1) %>% #deletions
+      filter(nchar(ALT)==2*str_count(ALT,",")+1) #insertions
+    alt_num <- str_count(output$ALT,",") + 1 #number of alternates
+    paired = which(alt_num!=1) #all with ,
+    #Add AF Column
+    af <- str_match(output$INFO, ';AF=([^;]*);')[,2] %>% strsplit(",") %>% sapply(as.numeric)
+    output <- data.frame(GENE = gene, "AF_1000G"=I(af), output) #Places it at the front of output
+    front_cols <- 1:(grep("HG00096",colnames(output))-1)
+    if (length(paired)!=0) {
+      #Limit max vector length by sapply(strsplit(output$ALT,","),length)
+      sapply(paired, function(rownum) { #For every row
+        sapply(as.character(1:alt_num[rownum]), function(num) {
+          grepl(paste0(num, "|"), output[rownum, -front_cols], fixed=T) +
+            grepl(paste0("|", num), output[rownum, -front_cols], fixed=T)
+        }) %>% t -> temp
+        split(temp, rep(1:ncol(temp), each = nrow(temp))) %>% setNames(NULL) 
+        #Separate into list of vectors (1 entry for counting each ALT)
+      }) %>% t -> insert
+      insert <- cbind(output[paired,front_cols],insert)
+      colnames(insert) <- colnames(output)
+      insert <- insert %>% #adds front_col info
+        mutate(ALT = strsplit(ALT,",")) %>% #Splits ALTS
+        unnest() %>% #Unnests everything
+        select(GENE, AF_1000G, CHROM, POS, ID, REF, ALT, everything()) #Reorders everything
+      output <- output[-paired,] #Removes paired
+    }
+    output <-  data.frame(output[,front_cols],
+                          apply(output[,-front_cols], 2, function(y) {
+                            grepl("1|", y, fixed=T) +
+                              grepl("|1", y, fixed=T)
+                          }) ) #convert to logical
+    if (length(paired)!=0)
+      output <- rbind(output, insert) #joins the two
+    output <- unite(output, VAR_ID, CHROM, POS, REF, ALT, sep = "_", remove = F) %>% 
+      arrange(VAR_ID) %>% mutate(AF_1000G = AF_1000G %>% unlist)
+    return(output)
   }
-  map <- system.file("extdata", "Supplementary_Files/phase3map.txt", package = "clinvaR") %>% 
-    read.table(stringsAsFactors = F, header = T) %>% as.data.frame
-  header <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", 
-              "FILTER", "INFO", "FORMAT", as.character(map$sample))
-  dir <- system.file("extdata", "", package = "clinvaR")
-  output <- read.table(sprintf('%s1000G/%s_genotypes.vcf', dir, gene), stringsAsFactors = FALSE)
-  #Add header
-  names(output)[1:length(header)] <- header
-  #Remove all single alt indels
-  output <- output[nchar(output$REF)==1,] #deletions
-  alt_num <- sapply(strsplit(output$ALT,","),length) #number of alts
-  acceptable_nchar <- 2*alt_num-1 #adds in the length from commas, if each alt is 1 nt.
-  output <- output[nchar(output$ALT)==acceptable_nchar,] #insertions
-  alt_num <- sapply(strsplit(output$ALT,","),length) #recalculate
-  paired = which(alt_num!=1) #all with ,
-  #Add AF Column
-  af <- strsplit(output$INFO,";") %>% sapply("[", 2) %>% 
-    strsplit("AF=") %>% sapply("[", 2) %>% strsplit(",") %>% sapply(as.numeric)
-  output <- cbind(GENE = gene, "AF_1000G"=I(af), output) #Places it at the front of output
-  front_cols <- 1:(grep("HG00096",colnames(output))-1)
-  if (length(paired)!=0) {
-    #Limit max vector length by sapply(strsplit(output$ALT,","),length)
-    sapply(paired, function(rownum) { #For every row
-      sapply(as.character(1:alt_num[rownum]), function(num) {
-        grepl(paste(num,"|",sep = ""), output[rownum,-front_cols], fixed=T) +
-          grepl(paste("|",num,sep = ""), output[rownum,-front_cols], fixed=T)
-      }) %>% t -> temp
-      split(temp, rep(1:ncol(temp), each = nrow(temp))) %>% setNames(NULL) 
-      #Separate into list of vectors (1 entry for counting each ALT)
-    }) %>% t -> insert
-    insert <- cbind(output[paired,front_cols],insert)
-    colnames(insert) <- colnames(output)
-    insert <- insert %>% #adds front_col info
-      mutate(ALT = strsplit(ALT,",")) %>% #Splits ALTS
-      unnest() %>% #Unnests everything
-      select(GENE, AF_1000G, CHROM, POS, ID, REF, ALT, everything()) #Reorders everything
-    output <- output[-paired,] #Removes paired
+  
+  dir <- system.file("extdata", package = "clinvaR")
+  map <- read.table(file = sprintf("%s/Supplementary_Files/phase3map.txt", dir), stringsAsFactors = F, header = T) %>% as.data.frame
+  header <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", as.character(map$sample))
+  combined <- NULL
+  for (gene in genes) {
+    combined <- rbind(combined, temp_function(gene))
   }
-  output <- cbind(output[,front_cols],
-                  apply(output[,-front_cols], 2, function(y) {
-                    grepl("1|", y, fixed=T) +
-                      grepl("|1", y, fixed=T)
-                  }) ) #convert to logical
-  if (length(paired)!=0)
-    output <- rbind(output, insert) #joins the two
-  output$AF_1000G <- as.numeric(output$AF_1000G)
-  unite(output, VAR_ID, CHROM, POS, REF, ALT, sep = "_", remove = F) %>% arrange(VAR_ID)
-  #Make VAR_ID, arrange by VAR_ID
+  combined <- combined[!duplicated(combined$VAR_ID),]
+  return(combined)
 }
