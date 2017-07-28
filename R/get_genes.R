311749e6446bf171259378b945661c1a28b4d1d7
James Diao <diao.james@gmail.com>
2017-07-28 15:00:05 -0400

    Updated get_genes() to display all MacArthur options

--- R/get_genes.R
+++ R/get_genes.R
@@ -1,12 +1,15 @@
 #' Imports List of Genes from File
 #'
 #' This function imports a list of genes from a specified file location, or from the MacArthur gene lists. 
-#' @usage get_genes(file)
+#' @usage get_genes()
+#' get_genes(file)
 #' @param file character; specified file location, or local file names from extdata/MacArthur_Gene_Lists/gene_lists/lists
 #' @examples get_genes('lmm_hcm.tsv')
 #' @export
 
 get_genes <- function(file) {
+  if (missing(file))
+    return(system(sprintf("ls %s", system.file('extdata/MacArthur_Gene_Lists/lists/', package = 'clinvaR')), intern = T))
   if (file.exists(file)) {
     return(read.table(file) %>% unlist() %>% unique() %>% as.character())
   } else {
@@ -19,6 +22,5 @@ get_genes <- function(file) {
     } else {
       stop('ERROR: file not found.')
     }
-    
   }
 }
