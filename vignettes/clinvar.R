de1be1b88403515837723bde4b2592b1daac35b3
James Diao <diao.james@gmail.com>
2017-08-01 17:02:55 -0400

    Troubleshooting vignette

---
+++ vignettes/clinvar.R
@@ -0,0 +1,4 @@
+## ----setup, include = F--------------------------------------------------
+knitr::opts_knit$set(root.dir = ".");
+knitr::opts_chunk$set(echo = T, eval = T, cache = T, warning = F, message = F)
+
